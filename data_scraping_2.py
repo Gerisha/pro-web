fb584edd9e3e55dc4d168529704c1294dfaadf21
Gerisha <75563368+Gerisha@users.noreply.github.com>
2023-01-06 21:04:42 +0530

    Update data_scraping_2.py

--- data_scraping_2.py
+++ data_scraping_2.py
@@ -40,4 +40,4 @@ for i in range(1,len(temp_list)):
 df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
 print(df2)
 
-df2.to_csv('dwarf_stars.csv')
+df2.to_csv('scapee.csv')
